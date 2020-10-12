f31a9c6d5d98cbfd9fa746779e6033e0db5341e2
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-10-12 19:19:20 -0400

    updated login screen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
   return (
