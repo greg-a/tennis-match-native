62a0a7daa9c7f9e6a71ddca82fadeef2588704aa
Greg Allebach <greg.allebach@gmail.com>
2020-11-10 19:05:14 -0500

    removed bottomstack from stack navigator to fix require cycle warning

--- client/app/navigation/DrawerNavigator.js
+++ client/app/navigation/DrawerNavigator.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { View, Text, Platform, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { MainStackNavigator, ProfileStackNavigator, AvailabilityStackNavigator, FeedStackNavigator, MessengerStackNavigator, CalendarStackNavigator, SearchByDateStackNavigator, RequestsStackNavigator } from "./StackNavigator";
+import { ProfileStackNavigator, MessengerStackNavigator, SearchByDateStackNavigator, RequestsStackNavigator } from "./StackNavigator";
 import BottomTabNavigator from "./TabNavigator";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    // <Drawer.Navigator>
-    //   <Drawer.Screen name="Profile" component={MainStackNavigator} />
-    //   <Drawer.Screen name="Availability" component={AvailabilityStackNavigator} />
-    //   <Drawer.Screen name="Feed" component={FeedStackNavigator} />
-    //   <Drawer.Screen name="Messenger" component={MessengerStackNavigator} />
-    //   <Drawer.Screen name="Calendar" component={CalendarStackNavigator} />
-    // </Drawer.Navigator>
 
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={BottomTabNavigator} 
@@ -48,29 +40,3 @@ const DrawerNavigator = () => {
 }
 
 export default DrawerNavigator;
-
-// SPLIT -----------------------------
-
-// import React from "react";
-
-// import { createDrawerNavigator } from "@react-navigation/drawer";
-
-// import { MainStackNavigator, ProfileStackNavigator, AvailabilityStackNavigator, FeedStackNavigator, MessengerStackNavigator, CalendarStackNavigator } from "./StackNavigator";
-// // import BottomTabNavigator from "./TabNavigator";
-
-// const Drawer = createDrawerNavigator();
-
-// const DrawerNavigator = () => {
-//   return (
-//     <Drawer.Navigator>
-//       <Drawer.Screen name="Profile" component={MainStackNavigator} />
-//       <Drawer.Screen name="Availability" component={AvailabilityStackNavigator} />
-//       <Drawer.Screen name="Feed" component={FeedStackNavigator} />
-//       <Drawer.Screen name="Messenger" component={MessengerStackNavigator} />
-//       <Drawer.Screen name="Calendar" component={CalendarStackNavigator} />
-//     </Drawer.Navigator>
-    
-//   );
-// }
-
-// export default DrawerNavigator;
--- client/app/navigation/StackNavigator.js
+++ client/app/navigation/StackNavigator.js
@@ -1,39 +1,18 @@
 import { View, Platform } from 'react-native';
-// import { Icon } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
 import MessengerScreen from '../screens/MessengerScreen';
 import FeedScreen from '../screens/FeedScreen';
 import AvailabilityScreen from '../screens/AvailabilityScreen';
-import LoginScreen from '../screens/LoginScreen';
 import React from 'react';
 import CalendarScreen from '../screens/CalendarScreen';
-import BottomTabNavigator from './TabNavigator';
 import InboxScreen from '../screens/InboxScreen';
 import UserSearchScreen from '../screens/UserSearchScreen';
 import SearchByDateScreen from '../screens/SearchByDateScreen';
-import SignUpScreen from '../screens/SignUpScreen';
 import SearchDateResultsScreen from '../screens/SearchDateResultsScreen';
 import SearchDatePropose from '../screens/SearchDatePropose';
 import RequestsScreen from '../screens/RequestsScreen';
-
-// const AuthStack = createStackNavigator();
-// const AuthStackScreen = () => (
-//   <AuthStack.Navigator>
-//     <AuthStack.Screen
-//       name="Login"
-//       component={LoginScreen}
-//       // options={{ title: "Login" }}
-//     />
-//     <AuthStack.Screen
-//       name="Create Account"
-//       component={SignUpScreen}
-//       // options={{ title: "Sign Up" }}
-//     />
-
-//   </AuthStack.Navigator>
-// )
 
 const Stack = createStackNavigator();
 
@@ -48,8 +27,6 @@ const MainStackNavigator = ({ navigation }) => {
         headerBackTitle: "Back",
       }}
     >
-      {/* <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
       <Stack.Screen name="Feed" component={FeedScreen} options={{
         headerLeft: () => (
           <View>
@@ -62,11 +39,6 @@ const MainStackNavigator = ({ navigation }) => {
           </View>
         ),
       }} />
-      <Stack.Screen name="Inbox" component={InboxScreen} />
-      <Stack.Screen name="Calendar" component={CalendarScreen} />
-      <Stack.Screen name="Messenger" component={MessengerScreen} />
-      {/* <Stack.Screen name="FindMatch" component={SearchByDateScreen} /> */}
-      <Stack.Screen name="Availability" component={AvailabilityScreen} />
     </Stack.Navigator>
 
   );
@@ -136,24 +108,6 @@ const CalendarStackNavigator = ({ navigation }) => {
 }
 
 export { CalendarStackNavigator };
-
-// const FeedStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Feed" component={FeedScreen} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { FeedStackNavigator };
 
 const ProfileStackNavigator = () => {
   return (
@@ -244,170 +198,3 @@ const RequestsStackNavigator = () => {
 export { RequestsStackNavigator };
   
 
-// SPLIT -------------------
-
-// import { Platform } from 'react-native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import ProfileScreen from '../screens/ProfileScreen';
-// import MessengerScreen from '../screens/MessengerScreen';
-// import FeedScreen from '../screens/FeedScreen';
-// import AvailabilityScreen from '../screens/AvailabilityScreen';
-// import LoginScreen from '../screens/LoginScreen';
-// import React from 'react';
-// import CalendarScreen from '../screens/CalendarScreen';
-// import SignUpScreen from '../screens/SignUpScreen';
-
-// // const PlacesNavigator = createStackNavigator(
-// //     {
-// //       Availability: AvailabilityScreen,
-// //       Calendar: {
-// //         screen: CalendarScreen
-// //       },
-// //       Profile: ProfileScreen,
-// //       Messenger: MessengerScreen
-// //     },
-// //     {
-// //       defaultNavigationOptions: {
-// //         headerStyle: {
-// //           backgroundColor: Platform.OS === 'android' ? 'rgb(108,230,49)' : ''
-// //         },
-// //         headerTintColor: Platform.OS === 'android' ? 'white' : 'rgb(108,230,49)'
-// //       }
-// //     }
-// // );
-
-// const Stack = createStackNavigator();
-
-// const MainStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
-//       <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown: false}} />
-//       <Stack.Screen name="Profile" component={ProfileScreen} />
-//       <Stack.Screen name="Availability" component={AvailabilityScreen} />
-//       <Stack.Screen name="Feed" component={FeedScreen} />
-//       <Stack.Screen name="Messenger" component={MessengerScreen} />
-//       <Stack.Screen name="Calendar" component={CalendarScreen} options={{headerShown: false}} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { MainStackNavigator };
-
-// const AvailabilityStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Availability" component={AvailabilityScreen} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { AvailabilityStackNavigator };
-
-// const FeedStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Feed" component={FeedScreen} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { FeedStackNavigator };
-
-// const ProfileStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Profile" component={ProfileScreen} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { ProfileStackNavigator };
-
-// const MessengerStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Messenger" component={MessengerScreen} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { MessengerStackNavigator };
-
-// const CalendarStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#6CE631",
-//       },
-//       headerTintColor: "white",
-//       headerBackTitle: "Back",
-//     }}
-//     >
-//       <Stack.Screen name="Calendar" component={CalendarScreen} options={{headerShown: false}} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { CalendarStackNavigator };
-
-// // const MainNavigator = createDrawerNavigator (
-// // {
-// //   Profile: {
-// //     screen: ProfileScreen,
-// //     navigationOptions: {
-// //       drawerLabel: 'Profile'
-// //     }
-// //   },
-// //   Availability: {
-// //     screen: AvailabilityScreen,
-// //     navigationOptions: {
-// //       drawerLabel: 'Availability'
-// //     }
-// //   }
-// // }
-// // );
-
-
-//   // export default createAppContainer({PlacesNavigator});
