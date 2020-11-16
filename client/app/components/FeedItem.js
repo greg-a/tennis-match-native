c22d44a5f190527d8ce647b3f67b6a1dcca43ff2
Patrick Urbankowski <purbankowski@gmail.com>
2020-11-15 19:18:32 -0500

    added updates to feed styling

--- client/app/components/FeedItem.js
+++ client/app/components/FeedItem.js
@@ -7,8 +7,7 @@ const FeedItem = props => {
             <TouchableOpacity onLongPress={props.onSelectEvent}>
                 <View>
                     <View>
-                        <Text>{props.organizer} scheduled a match with {props.confirmer}
-                        on {props.month}/{props.day} at {props.hour}.</Text>
+                        <Text style={styles.feedText}><Text style={styles.feedNames}>{props.organizer}</Text> scheduled a match with <Text style={styles.feedNames}>{props.confirmer} </Text>on {props.month}/{props.day} at {props.hour}.</Text>
                     </View>
                 </View>
             </TouchableOpacity>
@@ -18,13 +17,23 @@ const FeedItem = props => {
 
 const styles = StyleSheet.create({
     feedItem: {
-        height: 200,
+        height: 100,
         width: '100%',
         backgroundColor: '#f5f5f5',
-        borderRadius: 10,
+        // borderRadius: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e0e0e0',
         overflow: 'hidden',
         marginTop: 0
     },
+    feedText: {
+        fontSize: 15,
+        // paddingLeft: 5,
+        // paddingRight: 5
+    },
+    feedNames: {
+        fontWeight: 'bold',
+    }
 });
 
 export default FeedItem;
